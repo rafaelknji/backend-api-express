ccf729bf8bde1c14c165aeaefe009232c8bc6ded
rafaelknji <rafael.obara10@outlook.com>
2026-03-04 02:13:06 -0300

    Implement CRUD controllers for posts and update post router

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,23 +1,16 @@
 import express from 'express'
 import { getPostController } from '../controllers/post/getPostController.js'
 import { createPostController } from '../controllers/post/createPostController.js'
-
+import { updatePostController } from '../controllers/post/updatePostController.js'
+import { deletePostController } from '../controllers/post/deletePostController.js'
+import { patchPostController } from '../controllers/post/patchPostController.js'
 
 const router = express.Router()
 
 router.get('/', getPostController)
 router.post('/', createPostController)
-
-router.put('/', (req, res) => {
-  res.send('Exemplo de PUT na rota /post no Router')
-})
-
-router.patch('/', (req, res) => {
-  res.send('Exemplo de PATCH na rota /post no Router')
-})
-
-router.delete('/', (req, res) => {
-  res.send('Exemplo de DELETE na rota /post no Router')
-})
+router.put('/', updatePostController)
+router.patch('/', patchPostController)
+router.delete('/', deletePostController)
 
 export default router
